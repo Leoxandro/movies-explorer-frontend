f6ff5366451360a9d8f8418e36f22e10762369ad
Leoxandro <106742213+Leoxandro@users.noreply.github.com>
2024-02-19 23:33:27 +0300

    Delete movies-explorer-frontend directory

--- movies-explorer-frontend/src/components/TechIcon/TechIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./TechIcon.css";
-
-const TechIcon = ({ title }) => {
-  return <li className="tech-icon">{title}</li>;
-};
-
-export default TechIcon;
--- movies-explorer-frontend/src/utils/deleteKeyFromObj.js
+++
@@ -1,6 +0,0 @@
-export const deleteKeyFromObj = (object, keyName) => {
-    const result = Object.assign({}, object);
-    delete result[keyName];
-  
-    return result;
-  };
